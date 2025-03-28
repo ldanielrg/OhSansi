f17c4c62afda1fd9e7be7571a720dc5cb6f86a73
NaimMRez <thomas.mrez2002@gmail.com>
2025-03-28 15:59:53 -0400

    algunos de los campos del formulario de inscripcion creados

--- frontend/src/components/RegistroForm.jsx
+++ frontend/src/components/RegistroForm.jsx
@@ -1,11 +1,28 @@
 import React from 'react';
 import '../styles/RegistroForm.css';
 
-const RegistroForm = ({ label, placeholder, name }) => {
+const RegistroForm = ({ label, placeholder, name, type = 'text', options }) => {
     return (
         <div className="registro-form-field">
             <label htmlFor={name} className='nombre-registro'>{label}</label>
-            <input className='input-registro' type="text" name={name} placeholder={placeholder} />
+
+            {type === 'select' ? (
+                <select className="input-registro" name={name} id={name}>
+                    {options?.map((option, idx) => (
+                        <option key={idx} value={option.value}>
+                            {option.label}
+                        </option>
+                    ))}
+                </select>
+            ) : (
+                <input
+                    className="input-registro"
+                    type={type}
+                    name={name}
+                    placeholder={placeholder}
+                    id={name}
+                />
+            )}
         </div>
     );
 };
--- frontend/src/pages/Inscripciones.jsx
+++ frontend/src/pages/Inscripciones.jsx
@@ -16,11 +16,54 @@ const Inscripciones = () => {
                 <div className='contenedor-secciones-form'>
                     <section className='seccion-form'>
                         <RegistroForm label='Nombre Completo' placeholder='Juan Lopez' name='nombre'></RegistroForm>
+                        <RegistroForm label='Rude' name='rude'></RegistroForm>
+                        <RegistroForm 
+                            label='Provinvia' 
+                            name='provincia' 
+                            type='select' 
+                            options={[
+                                { value: '', label: 'Seleccione una provincia' },
+                                { value: 'arani', label: 'Arani' },
+                                { value: 'arque', label: 'Arque' },
+                                { value: 'ayopaya', label: 'Ayopaya' },
+                                { value: 'bolivar', label: 'Bolivar' },
+                                { value: 'capinota', label: 'Capinota' },
+                                { value: 'carrasco', label: 'Carrasco' },
+                                { value: 'cercado ', label: 'Cercado ' },
+                                { value: 'chapare', label: 'Chapare' },
+                                { value: 'esteban arce', label: 'Esteban Arce' },
+                                { value: 'german jordan', label: 'German Jordan' },
+                                { value: 'mizque', label: 'Mizque' },
+                                { value: 'narciso campero', label: 'Narciso Campero' },
+                                { value: 'punata', label: 'Punata' },
+                                { value: 'quillacollo', label: 'Quillacollo' },
+                                { value: 'tapacarí', label: 'Tapacarí' },
+                                { value: 'tiraque', label: 'Tiraque' }
+
+                            ]}
+                        />
+                        <RegistroForm label='CI/Pasaporte' name='ci/pasaporte'></RegistroForm>
+                        <RegistroForm 
+                        label='Año de escolaridad' 
+                        name='año de escolaridad' 
+                        type='select'
+                        options={[
+                            { value: '', label: 'Seleccione el curso' },
+                                { value: 'arani', label: 'Arani' },
+                                { value: 'arque', label: 'Arque' },
+                                { value: 'ayopaya', label: 'Ayopaya' },
+                                { value: 'bolivar', label: 'Bolivar' },
+                                { value: 'capinota', label: 'Capinota' },
+                                { value: 'carrasco', label: 'Carrasco' },
+                        ]}
+                        />
                     </section>
 
                     <section className='seccion-form'>
-                        <RegistroForm label='Fecha de nacimiento'></RegistroForm>
+                        <RegistroForm label='Fecha de nacimiento' type='date'></RegistroForm>
                     </section>
+
+
                 </div>
             </Caja>
         </div>
