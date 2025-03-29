d9ce1aa5850363a1b893edcccc647ef4f5a0f907
NaimMRez <thomas.mrez2002@gmail.com>
2025-03-29 00:40:45 -0400

    Formulario de cronograma creado dentro de Eventos

--- frontend/src/pages/Eventos.jsx
+++ frontend/src/pages/Eventos.jsx
@@ -1,12 +1,42 @@
 import React from 'react';
 import '../styles/Eventos.css';
 import Caja from '../components/Caja';
+import RegistroForm from '../components/RegistroForm';
 
 const Eventos = () => {
     return (
         <div className="page-container">
-            <Caja titulo="Eventos">
-                <p>Aquí se mostrarán los próximos eventos de las Olimpiadas Científicas.</p>
+            <Caja titulo="Crear nuevo evento">
+                <h2>Cronograma</h2>
+                <div className='contenedor-cronograma-convocatoria'>
+                    <section className='cont-form-cronograma'>
+                        <RegistroForm 
+                            label='Nombre del evento'
+                        />
+                        <RegistroForm
+                            label='Fecha de inicio'
+                            type='date'
+                        />
+                        <RegistroForm
+                            label='Fecha de Preinscripcion'
+                            type='date'
+                        />
+                        
+                    </section>
+                    <section className='cont-form-cronograma'>
+                        <RegistroForm 
+                            label='Duracion (dias)'
+                        />
+                        <RegistroForm
+                            label='Fecha fin'
+                            type='date'
+                        />
+                        <RegistroForm
+                            label='Fecha de Inscripcion'
+                            type='date'
+                        />
+                    </section>
+                </div>
             </Caja>
         </div>
     );
