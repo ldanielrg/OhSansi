cafcd087469f90f92583714a29fed332174967f0
ldanielrg <ldanielrg3@gmail.com>
2025-03-29 22:57:10 -0400

    Añadidos roles a RoleSeeder

--- backend/database/seeders/RoleSeeder.php
+++ backend/database/seeders/RoleSeeder.php
@@ -5,6 +5,9 @@ namespace Database\Seeders;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
+use Spatie\Permission\Models\Role;
+use Spatie\Permission\Models\Permission;
+
 class RoleSeeder extends Seeder
 {
     /**
@@ -12,6 +15,15 @@ class RoleSeeder extends Seeder
      */
     public function run(): void
     {
-        //
+        $role1 = Role::create(['name' => 'Admin']);
+        $role2 = Role::create(['name' => 'Director']);
+        $role3 = Role::create(['name' => 'Docente']);
+        $role4 = Role::create(['name' => 'Tutor']);
+        $role5 = Role::create(['name' => 'Inscrip']);
+        $role6 = Role::create(['name' => 'Caja']);
+        $role7 = Role::create(['name' => 'Organizador']);
+        $role8 = Role::create(['name' => 'Aux']);
+
+        Permission::create(['name' => 'edit articles']);
     }
 }
