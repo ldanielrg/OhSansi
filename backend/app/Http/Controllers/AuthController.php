307aa0b1a7cac3a20076d4421cdd971e2e0424c7
ldanielrg <ldanielrg3@gmail.com>
2025-03-30 23:17:20 -0400

    Creada función para borrar el token, en AuthController

--- backend/app/Http/Controllers/AuthController.php
+++ backend/app/Http/Controllers/AuthController.php
@@ -9,8 +9,7 @@ use App\Models\User;
 class AuthController extends Controller
 {
     public function login(Request $request)
-{
-    try {
+    {
         $request->validate([
             'email' => 'required|email',
             'password' => 'required'
@@ -29,12 +28,12 @@ class AuthController extends Controller
             'user' => $user,
             'roles' => $user->getRoleNames()
         ]);
-    } catch (\Throwable $e) {
-        return response()->json([
-            'error' => $e->getMessage(),
-            'line' => $e->getLine(),
-            'file' => $e->getFile()
-        ], 500);
     }
-}
+
+    public function logout(Request $request)
+    {
+        $request->user()->currentAccessToken()->delete();
+
+        return response()->json(['message' => 'Sesión cerrada']);
+    }
 }
