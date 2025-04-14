3f678e03f0d9410ea1d194eba1afa591816ef9aa
ldanielrg <ldanielrg3@gmail.com>
2025-04-14 17:56:20 -0400

    Arreglados errores en seeder. Completado llenado de BD

--- backend/database/seeders/CategoriaSeeder.php
+++ backend/database/seeders/CategoriaSeeder.php
@@ -1,37 +1,38 @@
 <?php
+namespace Database\Seeders;
 
 use Illuminate\Database\Seeder;
 use App\Models\Area;
 use App\Models\AreaTieneCategorium;
 use App\Models\Categorium;
 
-class AreaCategoriaSeeder extends Seeder
+class CategoriaSeeder extends Seeder
 {
     public function run(): void
     {
         // Crear áreas
-        $areaFisica = Area::updateOrCreate(
+        $areaFisica = Area::firstOrCreate(
             ['nombre_area' => 'Física'],
             ['activo' => true]
         );
 
         // Crear categorías
-        $cat4 = Categorium::updateOrCreate(['nombre_categoria' => '4S']);
-        $cat5 = Categorium::updateOrCreate(['nombre_categoria' => '5S']);
-        $cat6 = Categorium::updateOrCreate(['nombre_categoria' => '6S']);
+        $cat4 = Categorium::firstOrCreate(['nombre_categoria' => '4S']);
+        $cat5 = Categorium::firstOrCreate(['nombre_categoria' => '5S']);
+        $cat6 = Categorium::firstOrCreate(['nombre_categoria' => '6S']);
 
         // Relacionar áreas y categorías (vía tabla pivote)
-        AreaTieneCategorium::updateOrCreate([
+        AreaTieneCategorium::firstOrCreate([
             'id_area_area' => $areaFisica->id_area,
             'id_categoria_categoria' => $cat4->id_categoria,
         ]);
 
-        AreaTieneCategorium::updateOrCreate([
+        AreaTieneCategorium::firstOrCreate([
             'id_area_area' => $areaFisica->id_area,
             'id_categoria_categoria' => $cat5->id_categoria,
         ]);
 
-        AreaTieneCategorium::updateOrCreate([
+        AreaTieneCategorium::firstOrCreate([
             'id_area_area' => $areaFisica->id_area,
             'id_categoria_categoria' => $cat6->id_categoria,
         ]);
--- backend/database/seeders/DepartamentoSeeder.php
+++ backend/database/seeders/DepartamentoSeeder.php
@@ -12,38 +12,38 @@ class DepartamentoSeeder extends Seeder
     public function run(): void
     {
         // Crear departamentos
-        $dep1 = Departamento::updateOrCreate(['nombre_departamento' => 'Cochabamba']);
-        $dep2 = Departamento::updateOrCreate(['nombre_departamento' => 'Santa Cruz']);
-        $dep3 = Departamento::updateOrCreate(['nombre_departamento' => 'Beni']);
-        $dep4 = Departamento::updateOrCreate(['nombre_departamento' => 'Chuquisaca']);
-        $dep5 = Departamento::updateOrCreate(['nombre_departamento' => 'Tarija']);
-        $dep6 = Departamento::updateOrCreate(['nombre_departamento' => 'Pando']);
-        $dep7 = Departamento::updateOrCreate(['nombre_departamento' => 'La Paz']);
-        $dep8 = Departamento::updateOrCreate(['nombre_departamento' => 'Oruro']);
-        $dep9 = Departamento::updateOrCreate(['nombre_departamento' => 'Potosi']);
+        $dep1 = Departamento::firstOrCreate(['nombre_departamento' => 'Cochabamba']);
+        $dep2 = Departamento::firstOrCreate(['nombre_departamento' => 'Santa Cruz']);
+        $dep3 = Departamento::firstOrCreate(['nombre_departamento' => 'Beni']);
+        $dep4 = Departamento::firstOrCreate(['nombre_departamento' => 'Chuquisaca']);
+        $dep5 = Departamento::firstOrCreate(['nombre_departamento' => 'Tarija']);
+        $dep6 = Departamento::firstOrCreate(['nombre_departamento' => 'Pando']);
+        $dep7 = Departamento::firstOrCreate(['nombre_departamento' => 'La Paz']);
+        $dep8 = Departamento::firstOrCreate(['nombre_departamento' => 'Oruro']);
+        $dep9 = Departamento::firstOrCreate(['nombre_departamento' => 'Potosi']);
 
         // Relacionar los municipio a cochabamba (sólo cocha por ahora)
-        Municipio::updateOrCreate([
+        Municipio::firstOrCreate([
             'nombre_municipio' => 'Quillacollo',
             'id_departamento_departamento' => $dep1->id_departamento,
         ]);
-        Municipio::updateOrCreate([
+        Municipio::firstOrCreate([
             'nombre_municipio' => 'Sacaba',
             'id_departamento_departamento' => $dep1->id_departamento,
         ]);
-        Municipio::updateOrCreate([
+        Municipio::firstOrCreate([
             'nombre_municipio' => 'Tiquipaya',
             'id_departamento_departamento' => $dep1->id_departamento,
         ]);
-        Municipio::updateOrCreate([
+        Municipio::firstOrCreate([
             'nombre_municipio' => 'Colcapirhua',
             'id_departamento_departamento' => $dep1->id_departamento,
         ]);
-        Municipio::updateOrCreate([
+        Municipio::firstOrCreate([
             'nombre_municipio' => 'Vinto',
             'id_departamento_departamento' => $dep1->id_departamento,
         ]);
-        Municipio::updateOrCreate([
+        Municipio::firstOrCreate([
             'nombre_municipio' => 'Sipe Sipe,',
             'id_departamento_departamento' => $dep1->id_departamento,
         ]);
