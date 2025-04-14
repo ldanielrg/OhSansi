b8210451cb1cb907270b4c2727dd32be3f63a2cd
oscarin <oscarin@gmail.com>
2025-04-14 17:04:17 -0400

    Actualizamos funcionalidad

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,34 +1,15 @@
 // App.jsx
-import React from 'react'
-import { Routes, Route } from 'react-router-dom'
-import Home from './pages/Home'
-import Login from './pages/login'
-import Layout from './components/Layout'
+import React from 'react';
+import Layout from './components/Layout';
+import Home from './pages/Home';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
   return (
-    <Routes>
-      {/* Ruta Home ("/") envuelta en Layout */}
-      <Route
-        path="/"
-        element={
-          <Layout>
-            <Home />
-          </Layout>
-        }
-      />
-
-      {/* Ruta Login ("/login") también con Layout */}
-      <Route
-        path="/login"
-        element={
-          <Layout>
-            <Login />
-          </Layout>
-        }
-      />
-    </Routes>
-  )
+    <Layout>
+      <Home />
+    </Layout>
+  );
 }
 
-export default App
+export default App;
--- frontend/src/components/Layout.jsx
+++ frontend/src/components/Layout.jsx
@@ -1,10 +1,33 @@
 // Layout.jsx
 import React from "react";
 import "../styles/Layout.css";
-import { useNavigate } from 'react-router-dom'
+import { useNavigate } from "react-router-dom";
 
 const Layout = ({ children }) => {
-  const navigate = useNavigate()
+  const navigate = useNavigate();
+
+  // Funciones para cada ruta
+  const handleNavigateHome = (e) => {
+    e.preventDefault(); // Evita que <a> haga una recarga
+    navigate("/"); // Navega a "/"
+  };
+  const handleNavigateInscripciones = (e) => {
+    e.preventDefault();
+    navigate("/inscripciones");
+  };
+  const handleNavigateEventos = (e) => {
+    e.preventDefault();
+    navigate("/eventos");
+  };
+  const handleNavigateNosotros = (e) => {
+    e.preventDefault();
+    navigate("/nosotros");
+  };
+  const handleNavigateLogin = (e) => {
+    e.preventDefault();
+    navigate("/login");
+  };
+
   return (
     <div>
       {/* Top Bar */}
@@ -37,57 +60,81 @@ const Layout = ({ children }) => {
       {/* Navigation Bar */}
       <nav className="navbar custom-navbar">
         <div className="container d-flex justify-content-between align-items-center">
-          {/* Lista de botones de la izquierda (menú horizontal) */}
+          {/* Menú de la izquierda */}
           <ul className="navbar-nav d-flex flex-row" id="left-menu">
             <li className="nav-item me-3">
-            <Link className="nav-link text-white" to="#">
+              <a
+                className="nav-link text-white"
+                
+                onClick={handleNavigateHome}
+              >
                 <img
                   src="/src/assets/icono1.svg"
                   alt="Home Icon"
                   style={{ width: "20px", marginRight: "5px" }}
-                />{" "}
+                />
                 Inicio
-            </Link>
+              </a>
             </li>
             <li className="nav-item me-3">
-            <Link className="nav-link text-white" to="#">
-              <img
+              <a
+                className="nav-link text-white"
+                
+                onClick={handleNavigateInscripciones}
+              >
+                <img
                   src="/src/assets/icono2.svg"
-                  alt="Home Icon"
+                  alt="Inscripciones Icon"
                   style={{ width: "20px", marginRight: "5px" }}
-                />{" "} Inscripciones
-              </Link>
+                />
+                Inscripciones
+              </a>
             </li>
             <li className="nav-item me-3">
-            <Link className="nav-link text-white" to="#">
-              <img
+              <a
+                className="nav-link text-white"
+                
+                onClick={handleNavigateEventos}
+              >
+                <img
                   src="/src/assets/icono3.svg"
-                  alt="Home Icon"
+                  alt="Eventos Icon"
                   style={{ width: "20px", marginRight: "5px" }}
-                />{" "} Eventos
-              </Link>
+                />
+                Eventos
+              </a>
             </li>
             <li className="nav-item me-3">
-            <Link className="nav-link text-white" to="#">
-              <img
+              <a
+                className="nav-link text-white"
+                
+                onClick={handleNavigateNosotros}
+              >
+                <img
                   src="/src/assets/icono4.svg"
-                  alt="Home Icon"
+                  alt="Nosotros Icon"
                   style={{ width: "20px", marginRight: "5px" }}
-                />{" "} Nosotros
-              </Link>
+                />
+                Nosotros
+              </a>
             </li>
           </ul>
 
           {/* Botón "Ingresar" a la derecha */}
           <ul className="navbar-nav" id="right-menu">
             <li className="nav-item">
-            <Link className="nav-link text-white" to="#">
-              <img
+              <a
+                className="nav-link text-white"
+                
+                onClick={handleNavigateLogin}
+              >
+                <img
                   src="/src/assets/icono5.svg"
-                  alt="Home Icon"
+                  alt="Ingresar Icon"
                   style={{ width: "20px", marginRight: "5px" }}
-                />{" "} Ingresar
-              </Link>
+                />
+                Ingresar
+              </a>
             </li>
           </ul>
         </div>
