c025758c351922df3a921230646d4161140a8ed5
oscarin <oscarin@gmail.com>
2025-04-14 22:50:05 -0400

    Nuevas cosas

--- frontend/src/pages/login.jsx
+++ frontend/src/pages/login.jsx
@@ -5,7 +5,7 @@ export default function Login() {
   return (
     <div>
       <h2>Página de Login</h2>
-      <p>Tu formulario de acceso irá aquí más adelante.</p>
+      <p>Tu formulario de acceso irá aquí más adelantes.</p>
     </div>
   );
 }
