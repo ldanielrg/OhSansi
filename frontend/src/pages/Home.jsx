d35d4abcacd34ff3cdd44a9e60ac41f4a4a0d82d
oscarin <oscarin@gmail.com>
2025-04-21 20:05:46 -0400

    Cambios del jefe naim

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -62,7 +62,7 @@ const Home = () => {
     <div className="home-container container-fluid">
       <div className="row">
         {/* Columna Izquierda: Fondo y 4 Cards */}
-        <div className="col-md-9 left-side">
+        <div className="col-md-8 left-side">
           <div className="background-section">
             {/* Asigna aquí tu imagen de fondo o color */}
           </div>
@@ -99,7 +99,7 @@ const Home = () => {
         </div>
 
         {/* Columna Derecha: Un card grande con Calendario, Noticias y Ayuda */}
-        <div className="col-md-3 right-side">
+        <div className="col-md-4 right-side">
           <div className="card long-card">
             <div className="card-body">
               {/* Calendario */}
