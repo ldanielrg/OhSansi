798c9fdb2efe689662b6f46a247053aeeab4cb24
oscarin <oscarin@gmail.com>
2025-04-23 11:01:56 -0400

    Actualizamos y mejoramos el css de eventos

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -163,6 +163,22 @@ function App() {
         }
       />
       <Route
+        path="/crear-evento"
+        element={
+          <Layout>
+            <CrearEvento />
+          </Layout>
+        }
+      />
+      <Route
+        path="/editar-evento"
+        element={
+          <Layout>
+            <EditarEvento />
+          </Layout>
+        }
+      />
+      <Route
         path="/login"
         element={
           <Layout>
