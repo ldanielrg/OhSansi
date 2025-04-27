922f7ce51fb3289a8f82bbfd2a96ba39e58de8d3
NaimMRez <thomas.mrez2002@gmail.com>
2025-04-26 23:26:22 -0400

    arreglo de posicion de codigo

--- frontend/src/pages/CamposModificarCuenta.jsx
+++ frontend/src/pages/CamposModificarCuenta.jsx
@@ -6,77 +6,77 @@ import BotonForm from '../components/BotonForm';
 import { useAuth } from "../context/AuthContext"; 
 
 const ModificarCuenta = () => {
-  const [usuario, setUsuario] = useState({
-    nombreCuenta: "Juan Pérez",
-    email: "juan@example.com",
-    password: "123456",
-  });
+    const [usuario, setUsuario] = useState({
+        nombreCuenta: "Juan Pérez",
+        email: "juan@example.com",
+        password: "123456",
+    });
 
-  const {
-    register,
-    handleSubmit,
-    setValue,
-    formState: { errors },
-  } = useForm();
+    const {
+        register,
+        handleSubmit,
+        setValue,
+        formState: { errors },
+    } = useForm();
 
-  useEffect(() => {
-    setValue("nombreCuenta", usuario.nombreCuenta);
-    setValue("email", usuario.email);
-    setValue("password", usuario.password);
-  }, [usuario, setValue]);
+    useEffect(() => {
+        setValue("nombreCuenta", usuario.nombreCuenta);
+        setValue("email", usuario.email);
+        setValue("password", usuario.password);
+    }, [usuario, setValue]);
 
-  const onSubmit = (data) => {
-    const confirmar = window.confirm("¿Estás seguro de que deseas guardar los cambios?");
-    if (confirmar) {
-      setUsuario(data);
-      alert("¡Datos actualizados con éxito!");
-    }
-  };
+    const onSubmit = (data) => {
+        const confirmar = window.confirm("¿Estás seguro de que deseas guardar los cambios?");
+        if (confirmar) {
+        setUsuario(data);
+        alert("¡Datos actualizados con éxito!");
+        }
+    };
 
-  return (
-    <div className="page-container-modificar-cuenta">
-      <section className="seccion-formulario-modificar-cuenta">
-        <h2>Modificacion de la cuenta</h2>
-        <div className="cont-form-mod">
+    return (
+        <div className="page-container-modificar-cuenta">
+        <section className="seccion-formulario-modificar-cuenta">
+            <h2>Modificacion de la cuenta</h2>
+            <div className="cont-form-mod">
 
-        
-        <form onSubmit={handleSubmit(onSubmit)}>
-          <div className="div-label-input-modificar-cuenta">
-            <RegistroForm label='Nombre'>
+            
+            <form onSubmit={handleSubmit(onSubmit)}>
+            <div className="div-label-input-modificar-cuenta">
+                <RegistroForm label='Nombre'>
+
+                </RegistroForm>
+            </div>
+            {errors.nombreCuenta && <p>{errors.nombreCuenta.message}</p>}
+
+            <div className="div-label-input-modificar-cuenta">
+            <RegistroForm label='email'>
+
+                </RegistroForm>
+            </div>
+            {errors.email && <p>{errors.email.message}</p>}
+
+            <div className="div-label-input-modificar-cuenta">
+            <RegistroForm label='contraseña'>
 
             </RegistroForm>
-          </div>
-          {errors.nombreCuenta && <p>{errors.nombreCuenta.message}</p>}
+            </div>
+            {errors.password && <p>{errors.password.message}</p>}
 
-          <div className="div-label-input-modificar-cuenta">
-          <RegistroForm label='email'>
+            <div className="div-label-input-modificar-cuenta">
+            <RegistroForm label='Confirmar contraseña'>
 
             </RegistroForm>
-          </div>
-          {errors.email && <p>{errors.email.message}</p>}
+            </div>
+            <div className="div-label-input-modificar-cuenta">
+                <BotonForm className='ddddddd' texto= 'Volver'type="submit"></BotonForm>
+                <BotonForm texto= 'Confirmar'type="submit"></BotonForm>
+            </div>
+            </form>
+            </div>
 
-          <div className="div-label-input-modificar-cuenta">
-          <RegistroForm label='contraseña'>
-
-          </RegistroForm>
-          </div>
-          {errors.password && <p>{errors.password.message}</p>}
-
-          <div className="div-label-input-modificar-cuenta">
-          <RegistroForm label='Confirmar contraseña'>
-
-          </RegistroForm>
-          </div>
-          <div className="div-label-input-modificar-cuenta">
-            <BotonForm className='ddddddd' texto= 'Volver'type="submit"></BotonForm>
-            <BotonForm texto= 'Confirmar'type="submit"></BotonForm>
-          </div>
-        </form>
+        </section>
         </div>
-
-      </section>
-    </div>
-  );
+    );
 };
 
 export default ModificarCuenta;
